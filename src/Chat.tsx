bbe06a0b2635675aeadef4af4c4dab4e7fcb5a43
yokowasis <yokowasis@gmail.com>
2021-10-11 23:11:19 +0800

    sending chat success

---
+++ src/Chat.tsx
@@ -0,0 +1,211 @@
+import { useEffect, useState } from 'react';
+import Gun from 'gun';
+import 'gun/sea';
+
+const gun = Gun("https://fire-gun.herokuapp.com/gun");
+
+export function Chat(props:{[keys:string] : any}) {
+
+    const [myPairKey, setMyPairKey] = useState({epub : "",pub : "",priv : "",epriv : ""})
+    const [yourPairKey, setYourPairKey] = useState({pub : "",epub : ""})
+    const [yourCertificate, setYourCertificate] = useState("")
+    const [pairKey, setPairKey] = useState("")
+    const [partnerKey, setPartnerKey] = useState("")
+    const [keterangan, setKeterangan] = useState("")
+    const [keterangan2, setKeterangan2] = useState("")
+    const [chatsMessages, setChatsMessages] = useState<{_self : boolean, msg: string, timestamp : string }[]>([])
+    const [chatMessagesDiv, setChatMessagesDiv] = useState<JSX.Element>(<></>)
+    const [textMsg, setTextMsg] = useState("");
+    const [partnerKeyStateReadOnly, setPartnerKeyStateReadOnly] = useState(false);
+    
+    
+    const loginPair = async () => {
+        let pairKey = await Gun.SEA.pair()
+        setPairKey(`${pairKey?.pub}&${pairKey?.epub}`)
+        setMyPairKey({priv : (pairKey?.priv || ""), pub: (pairKey?.pub || ""), epriv : (pairKey?.epriv || ""), epub : (pairKey?.epub || "") })
+
+        // Generate Certificate
+        let cert = await (Gun as any).SEA.certify("*", [{ "*" : "chat-with","+" : "*"}], pairKey,null,{
+            // block : 'chat-blacklist' //ADA BUG DARI GUN JADI BELUM BISA BLACKLIST
+        });
+        gun.user().auth(pairKey as any,()=>{
+            gun.user().get("chat-cert").put(cert,(s=>{
+                if (s.err) {
+                    console.log ("Error Creating Certificate")
+                } else {
+                    console.log ("Success Creating Certificate")
+                }
+            }));
+        })
+        
+
+        setKeterangan("Login Berhasil");
+    }
+
+    const pairInputClick = async () => {
+        await window.navigator.clipboard.writeText(pairKey);
+        setKeterangan("Pairkey di copy ke clipboard");
+    }
+    
+    const partnerInputClick = async () => {
+    }
+
+    const send = async (callback:()=>void) => {
+        let msgToHim, msgToMe;
+        if (yourPairKey.epub) {
+            msgToHim = await Gun.SEA.encrypt(textMsg,await (Gun as any).SEA.secret(yourPairKey.epub, myPairKey));
+            msgToMe = await Gun.SEA.encrypt(textMsg,myPairKey);
+        } else {
+            msgToHim = textMsg
+        }
+        let cert = yourCertificate;
+        let currentdate = new Date(); 
+        let year = currentdate.getFullYear();
+        let month  = ((currentdate.getMonth()+1) < 10) ? "0" + (currentdate.getMonth()+1) : (currentdate.getMonth()+1);
+        let date = (currentdate.getDate() < 10) ? "0" + (currentdate.getDate()) : (currentdate.getDate());
+        let hour = (currentdate.getHours() < 10) ? "0" + (currentdate.getHours()) : (currentdate.getHours());
+        let minutes = (currentdate.getMinutes() < 10) ? "0" + (currentdate.getMinutes()) : (currentdate.getMinutes());
+        let seconds = (currentdate.getSeconds() < 10) ? "0" + (currentdate.getSeconds()) : (currentdate.getSeconds());
+        let miliseconds = (currentdate.getMilliseconds() < 10) ? "0" + (currentdate.getMilliseconds()) : (currentdate.getMilliseconds());
+        let datetime = `${year}/${month}/${date}T${hour}:${minutes}:${seconds}.${miliseconds}`;
+
+        console.log ("Send to Him ...")
+        gun.get(`~${yourPairKey.pub}`).get("chat-with").get(myPairKey.pub).get(year).get(month).get(date).put({
+            "_self" : false,
+            "timestamp" : datetime, 
+            "msg" : msgToHim, 
+        },(ack=>{
+            if (ack.err) {
+                console.log ("Send to Him ... Failed")
+            } else {
+                console.log ("Send to Him ... Success")
+                callback();
+            }
+        }),{
+            opt : {
+                cert : cert
+            }
+        })
+
+        console.log ("Send to Me ...")
+        gun.user().get("chat-with").get(yourPairKey.pub).get(year).get(month).get(date).put({
+            "_self" : true,
+            "timestamp" : datetime, 
+            "msg" : msgToMe, 
+        },(ack=>{
+            if (ack.err) {
+                console.log ("Send to Me ... Failed")
+            } else {
+                console.log ("Send to Me ... Success")
+            }
+        }))
+    }
+
+    const sendChat = async() => {
+        let s = chatsMessages;        
+        s.push({
+            msg : textMsg,
+            timestamp : "123123123",
+            _self : true,
+        })
+        setChatsMessages(s);
+        setTextMsg("")
+        console.log ("Sending Chat...")
+        send(()=>{
+            console.log ("Sending Chat... Success")
+        })        
+    }
+
+    const getCertificate = (tries:number) => {
+        let keys = partnerKey.split("&");
+        setYourPairKey({pub : keys[0], epub:keys[1]})
+
+        console.log ("Getting Certificate...")
+        gun.get(`~${keys[0]}`).get("chat-cert").once(s=>{
+            if (s) {
+                console.log ("Getting Certificate... Success");
+                console.log (s);
+                setYourCertificate(s as any);
+                setPartnerKeyStateReadOnly(true);
+            } else {
+                console.log (`Getting Certificate... Failed... Retry (${tries})`);
+                getCertificate(tries-1);
+            }
+        })
+    }
+
+    useEffect(()=>{
+        if (partnerKey !== "") {
+            getCertificate(10);
+        }
+        
+    },[partnerKey])
+
+    useEffect(()=>{
+        let i = 0;
+        setChatMessagesDiv(
+            <>
+                {
+                    chatsMessages.map((val)=>
+                        <div key={i++} className={`card col-md-7 mb-3 ${val._self ? "text-start bg-primary text-white" : "text-end offset-md-5"}`}>
+                            <img className="card-img-top" src="holder.js/100px180/" alt="" />
+                            <div className="card-body">
+                            <p className="card-text">{val.msg}</p>
+                            </div>
+                        </div>
+                    )
+                }
+            </>
+        )        
+    },[chatsMessages[chatsMessages.length-1]])
+
+    useEffect(()=>{
+        (window as any).gun = gun
+    },[])
+
+    return (
+        <>
+            <div className="row mt-3">
+                <div className="col ps-5">
+                    <div className="card text-start mb-3">
+                      <img className="card-img-top" src="holder.js/100px180/" alt="" />
+                      <div className="card-body">
+                        <h4 className="card-title">Chat Area</h4>
+                        <div className="card-text">
+                            {chatMessagesDiv}
+                        </div>
+                      </div>
+                    </div>
+                    <input value={textMsg} onChange={(e)=>{setTextMsg(e.target.value)}} className="form-control" onKeyPress={(e)=>{if (e.code === "Enter") {sendChat()}}} />
+                </div>
+                <div className="col text-start pe-5">
+                    <input onClick={loginPair} name="loginBtn" id="loginBtn" className="btn btn-primary mb-3" type="button" value="Login" />
+                    <div className="card mb-4">
+                      <img className="card-img-top" src="holder.js/100px180/" alt="" />
+                      <div className="card-body">
+                        <h4 className="card-title">My PairKey</h4>
+                        <div className="card-text">
+                            <input onClick={pairInputClick}  type="text" readOnly={true} value={pairKey}
+                                className="form-control" name="pairKey" id="pairKey" aria-describedby="pairKey" placeholder="Pairkey" />
+                            <small id="pairKey" className="form-text text-muted">Paste into Other Chat pairkey</small>
+                            <p><small className="form-text text-success">{keterangan}</small></p>
+                        </div>
+                      </div>
+                    </div>
+                    <div className="card">
+                      <img className="card-img-top" src="holder.js/100px180/" alt="" />
+                      <div className="card-body">
+                        <h4 className="card-title">Partner PairKey</h4>
+                        <div className="card-text">
+                            <textarea rows={5} onChange={(e)=>{setPartnerKey(e.target.value)}} onClick={partnerInputClick} readOnly={partnerKeyStateReadOnly}
+                                className="form-control" name="partnerKey" id="partnerKey" aria-describedby="partnerKey" placeholder="Paste Key Here" value={partnerKey}></textarea>
+                            <small id="pairKey" className="form-text text-muted">Get from other partner</small>
+                            <p><small className="form-text text-success">{keterangan2}</small></p>
+                        </div>
+                      </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
