ecaa575d29ab20230dcacf391b8ee879910cb5d8
yokowasis <yokowasis@gmail.com>
2021-10-17 16:57:54 +0800

    config

--- vite.config.ts
+++ vite.config.ts
@@ -6,8 +6,8 @@ require('dotenv').config();
 // https://vitejs.dev/config/
 export default defineConfig({
   server : {
-    port : parseInt(process.env.PORT)
+    port : parseInt(process.env.PORT || "3000")
   },
-  base : process.env.BASE_DIR,
+  base : process.env.BASE_DIR || "",
   plugins: [react()]
 })
