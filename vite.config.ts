a433dd4ec7e4edfd5809a3f0540ae212f565d182
yokowasis <yokowasis@gmail.com>
2021-10-17 16:37:45 +0800

    update config

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+require('dotenv').config();
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
   server : {
-    port : 3000
+    port : parseInt(process.env.PORT)
   },
+  base : process.env.BASE_DIR,
   plugins: [react()]
 })
