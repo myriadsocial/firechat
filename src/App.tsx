d0fc0e82934318310da5f4c244211b328cbd5747
yokowasis <yokowasis@gmail.com>
2021-10-18 19:51:05 +0800

    base UI

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,16 @@ import {
   Link,
   HashRouter,
 } from "react-router-dom";
+import { ChatMUI } from './ChatMUI';
+
+import { Firegun, Chat as ChatFG } from './firegun/firegun'
+
+const fg = new Firegun([
+  "https://gundb.dev.myriad.systems/gun", 
+  "https://gun-relay.bimasoft.web.id:16902/gun"
+])
+
+const chat = new ChatFG(fg)
 
 export default function App() {
   return (
@@ -18,12 +28,18 @@ export default function App() {
           <Route path="/chat">
             <Chat />
           </Route>
+          <Route path="/chatMUI">
+            <div style={{width : "500px", height:"600px",  margin: "auto"}}>
+              <ChatMUI fg={fg} chat={chat} />
+            </div>
+          </Route>
           <Route path="/iris">
             <Iris />
           </Route>
           <Route path="/">
             <p>Index Page :</p>
             <p><Link to="/chat">Chat</Link></p>
+            <p><Link to="/chatMUI">Chat Material UI</Link></p>
             <p><Link to="/iris">Iris</Link></p>
           </Route>
         </Switch>
