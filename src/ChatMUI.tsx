ddd886d0d33f35d597897785e4952dfbedb2bbc3
yokowasis <yokowasis@gmail.com>
2021-10-19 09:44:55 +0800

    save cert and map on

--- src/ChatMUI.tsx
+++ src/ChatMUI.tsx
@@ -1,7 +1,7 @@
 import { Chat, Firegun } from './firegun/firegun'
 import Grid from '@mui/material/Grid'
 import TextField from '@mui/material/TextField'
-import { useEffect, useState } from 'react'
+import { useCallback, useEffect, useRef, useState } from 'react'
 import Button from '@mui/material/Button'
 import { Send, AttachFile, FiveG } from '@mui/icons-material'
 import { Divider } from '@mui/material'
@@ -15,28 +15,74 @@ type ChatMUIProps = {
 
 export function ChatMUI(props:ChatMUIProps) {
 
-    const [textMsg, settextMsg] = useState("");
-    const [yourCertificate, setyourCertificate] = useState("");
-    const [yourPub, setyourPub] = useState("");
-    const [yourEpub, setyourEpub] = useState("");
+    const [textMsg, setTextMsg] = useState("");
+    const [yourCertificate, setYourCertificate] = useState("BBBBB");
+
+    var 
+        yourPub : string,
+        yourEpub : string,
+        yourCert:string;
 
     useEffect(()=>{
         // INIT FIRST TIME ONLY
-        var keys;
+
+        setYourCertificate("CCCC");
+
+        var keys:string[];
         if (props.partnerKey.indexOf("&")>=0) {
             keys = props.partnerKey.split("&")
             props.chat.getCert(keys[0])
             .then(cert=>{
-                setyourCertificate(cert || "");
-                console.log (cert);
+                yourCert = cert || "";
+                console.log (yourCert);
             })
-            setyourPub(keys[0])
-            setyourEpub(keys[1])
+            yourPub = keys[0];
+            yourEpub = keys[1];
+
+            let dateNow = getDate()
+            props.fg.gun.user().get("chat-with").get(yourPub).get(dateNow.year).get(dateNow.month).get(dateNow.date).map().once(async (s)=>{
+                if (s) {
+                    console.log (s);
+                    // processChat(s,keys);
+                }                        
+            })
         } else {
             console.log ("Partner Key Incomplete")
         }
     },[])
 
+    const processChat = async (s:{[x:string] : any},keys:string[]) => {
+        if ((s.msg as string).search("SEA") === 0)
+        if (s._self) {
+            s.msg = await Gun.SEA.decrypt(s.msg, this.state.myPairKey);
+        } else {
+            s.msg = await Gun.SEA.decrypt(s.msg, await (Gun as any).SEA.secret(keys[1], this.state.myPairKey));
+        }
+
+        let chatsTemp = this.state.chatsMessages;
+        chatsTemp = chatsTemp.filter(function( obj ) {
+            console.log (obj);
+            return obj.timestamp !== 'sending...';
+        });
+        chatsTemp.push({_self : s._self, msg : s.msg, timestamp: s.timestamp});
+        chatsTemp.sort(dynamicSort("timestamp"));
+        this.setState({
+            chatsMessages : chatsTemp
+        },this.updateChatDiv)
+    }
+
+    const getDate = () => {
+        let currentdate = new Date(); 
+        let year = currentdate.getFullYear();
+        let month  = ((currentdate.getMonth()+1) < 10) ? "0" + (currentdate.getMonth()+1) : (currentdate.getMonth()+1);
+        let date = (currentdate.getDate() < 10) ? "0" + (currentdate.getDate()) : (currentdate.getDate());
+        let hour = (currentdate.getHours() < 10) ? "0" + (currentdate.getHours()) : (currentdate.getHours());
+        let minutes = (currentdate.getMinutes() < 10) ? "0" + (currentdate.getMinutes()) : (currentdate.getMinutes());
+        let seconds = (currentdate.getSeconds() < 10) ? "0" + (currentdate.getSeconds()) : (currentdate.getSeconds());
+        let miliseconds = (currentdate.getMilliseconds() < 10) ? "0" + (currentdate.getMilliseconds()) : (currentdate.getMilliseconds());
+        return ( {year : year, month : month, date : date, hour : hour, minutes : minutes, seconds : seconds, miliseconds : miliseconds} )
+    }
+    
     return (
         <>
             <Grid
@@ -47,6 +93,7 @@ export function ChatMUI(props:ChatMUIProps) {
             >
                 <Grid item height='10%' textAlign="center" fontWeight="bold">
                     {props.partnerKey.slice(0,8)}
+                    {yourCertificate}
                     <Divider />
                 </Grid>
                 <Grid item height='70%' style={{overflowY : "scroll"}}>
