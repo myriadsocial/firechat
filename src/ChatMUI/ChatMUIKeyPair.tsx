f5ea0d0f1213fbd6240f2fcab053d8812bae4a96
yokowasis <yokowasis@gmail.com>
2021-10-20 12:36:48 +0800

    paper chat

--- src/ChatMUI/ChatMUIKeyPair.tsx
+++ src/ChatMUI/ChatMUIKeyPair.tsx
@@ -1,10 +1,25 @@
-import { Button, Divider, Grid, TextareaAutosize, TextField, Typography } from "@mui/material"
+import { Button, Divider, Grid, Paper, TextareaAutosize, TextField, Typography, CardMedia, useTheme } from "@mui/material"
+import { makeStyles } from "@mui/styles"
 import { useState } from "react"
 
 export default function ChatMUIKeyPair (props:{
     myPubKey:string,
     setPartners: React.Dispatch<React.SetStateAction<string[]>>
 }) {
+
+    const theme = useTheme()
+
+    const useStyles = makeStyles({
+        selfCard: {
+            backgroundColor : `${theme.palette.primary.main} !important`,
+            color : `${theme.palette.common.white} !important`,
+            padding : "10px",
+            maxWidth : "100%",
+            width : "300px"
+        },
+    });
+    
+    const classes = useStyles();
 
     const [text,setText] = useState("")
     const [partners, setpartners] = useState<string[]>([])
@@ -17,6 +32,14 @@ export default function ChatMUIKeyPair (props:{
     
     return (
         <>
+            
+            <Grid mb={3}>
+                <Paper className={classes.selfCard} elevation={4}>
+                    <Typography variant="body2">
+                        This is a text
+                    </Typography>
+                </Paper>
+            </Grid>
             <Grid mb={3} container direction="column" spacing={2} alignItems="center">
                 <Grid item width="100%" textAlign="center">
                     <Typography>My Pairkey:</Typography>
