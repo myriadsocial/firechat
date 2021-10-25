7dba91e6a9405b8c5bc81ef180c069074b9a199f
yokowasis <yokowasis@gmail.com>
2021-10-25 19:11:09 +0800

    index

--- index.tsx
+++ index.tsx
@@ -3,11 +3,12 @@ import {Firegun, Chat as ChatFG} from "@yokowasis/firegun"
 
 export default function Chat(props:{
     fg : Firegun,
-    chat : ChatFG
+    chat : ChatFG,
+    newChat? : string,
 }) {
     return(
         <>
-            <ChatMUIContainer fg={props.fg} chat={props.chat} />
+            <ChatMUIContainer fg={props.fg} chat={props.chat} newChat={props.newChat} />
         </>
     )
 }
