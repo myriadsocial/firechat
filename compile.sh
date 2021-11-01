eb8f49ab2243d7c2b28fba2558854090c11e0cae
yokowasis <yokowasis@gmail.com>
2021-11-01 19:03:11 +0800

    dynamic dependencies

--- compile.sh
+++ compile.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-bash clean.sh
 tsc index.tsx src/Chat/*.tsx --jsx react-jsx --esModuleInterop --lib "dom,dom.iterable,esnext"
+sed -i 's/\"\@yokowasis\/firegun\"\: \"file\:\.\.\/firegun\"/\"\@yokowasis\/firegun\" : \"@latest\" /g' ./package.json
+npm publish
+sed -i 's/\"\@yokowasis\/firegun\" : \"@latest\"/\"\@yokowasis\/firegun\"\: \"file\:\.\.\/firegun\" /g' ./package.json
+bash clean.sh
