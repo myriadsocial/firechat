4a7c7dc1b5d1e7b556d44a8e24822787a7e5d05b
yokowasis <yokowasis@gmail.com>
2021-11-02 14:18:15 +0800

    common

--- index.tsx
+++ index.tsx
@@ -1,14 +1,15 @@
 import ChatMUIContainer from "./src/Chat/ChatMUIContainer"
-import {Firegun, Chat as ChatFG} from '@yokowasis/firegun'
+import {Firegun, Chat as ChatFG, common} from '@yokowasis/firegun'
 
 export default function Chat(props:{
     fg : Firegun,
     chat : ChatFG,
     newChat? : string,
+    common : typeof common,
 }) {
     return(
         <>
-            <ChatMUIContainer fg={props.fg} chat={props.chat} newChat={props.newChat} />
+            <ChatMUIContainer common={props.common} fg={props.fg} chat={props.chat} newChat={props.newChat} />
         </>
     )
 }
